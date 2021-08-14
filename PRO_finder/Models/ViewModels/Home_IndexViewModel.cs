89aa5bf2b014c8bb1204aed34fcc76605e75dbcb
Sean <yoyo4116@gmail.com>
2021-08-14 21:25:06 +0800

    OK

---
+++ PRO_finder/Models/ViewModels/Home_IndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PRO_finder.Models.ViewModels
+{
+    public class Home_IndexViewModel
+    {
+        public List<CaseViewModel> MyProperty { get; set; }
+
+
+
+
+
+        
+    }
+
+   
+}
