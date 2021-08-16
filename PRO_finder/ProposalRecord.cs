465cf98f7d9e01dedcb243569b81e4be46e9b026
Chi-Ling-Lee <19940818jillian@gmail.com>
2021-08-16 12:14:51 +0800

    maybe not

---
+++ PRO_finder/ProposalRecord.cs
@@ -0,0 +1,25 @@
+namespace PRO_finder
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("ProposalRecord")]
+    public partial class ProposalRecord
+    {
+        public int MemberID { get; set; }
+
+        public int CaseID { get; set; }
+
+        public TimeSpan SavedDate { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ProposalRecordID { get; set; }
+
+        public virtual Case Case { get; set; }
+
+        public virtual MemberInfo MemberInfo { get; set; }
+    }
+}
