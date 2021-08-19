4d6fccdfb20c4f7aa323b06a13025daded803d68
LAPTOP-AUAKKL4K\wan <maggie42150@gmail.com>
2021-08-19 21:26:22 +0800

    0819

--- PRO_finder/Controllers/QuotationController.cs
+++ PRO_finder/Controllers/QuotationController.cs
@@ -4,17 +4,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using PRO_finder.Models;
+using PRO_finder.Models.ViewModels;
+using PRO_finder.Repositories;
+using PRO_finder.Service;
+using PRO_finder.Models.DBModel;
 
 namespace PRO_finder.Controllers
 {
-    
+
     public class QuotationController : Controller
     {
-        private ProFinderContext context = new ProFinderContext();
-        // GET: Quotation
-        public ActionResult Index()
+        private readonly QuotationService _quotService;
+
+        public QuotationController()
         {
-            return View();
+
+            _quotService = new QuotationService();
+        }
+
+        //ProFinderModels ctx = new ProFinderModels();
+        // GET: Quotation
+        public ActionResult Index(int CategoryId = 0)
+        {
+            //string Contain = this.TempData["Contain"] as string;
+
+            List<QuotationViewModel> pageData = _quotService.GetCategoryPageData(CategoryId);
+            ViewBag.cateNameList = _quotService.GetsubcatrgotyName(CategoryId);
+
+
+            return View(pageData);
         }
         public ActionResult Detail()
         {
@@ -31,5 +51,11 @@ namespace PRO_finder.Controllers
             return View();
         }
 
+        
+        public ActionResult AllcardData()
+        {
+            List<QuotationViewModel> allCardData = _quotService.GetAllCardData();
+            return Json(allCardData, JsonRequestBehavior.AllowGet);
+        }
     }
 }
