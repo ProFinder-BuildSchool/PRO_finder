f4f259f223698e0de90e204a2f4573514aac5b72
LAPTOP-AUAKKL4K\wan <maggie42150@gmail.com>
2021-08-20 23:29:45 +0800

    StudioService80%

--- PRO_finder/Service/StudioService.cs
+++ PRO_finder/Service/StudioService.cs
@@ -11,6 +11,7 @@ using System.Data.Entity.Spatial;
 using PRO_finder.Repositories;
 using PRO_finder.Model.ViewModels;
 using System.Data.Entity;
+using PRO_finder.Models.ViewModels;
 
 namespace PRO_finder.Service
 {
@@ -59,17 +60,87 @@ namespace PRO_finder.Service
                        YearStarted = works.YearStarted,
                        WebsiteURL = works.WebsiteURL,
                        SubCategoryID = works.SubCategoryID,
-                       MemberID=memberinfo.MemberID,
-                       //ProfilePicture=memberinfo.ProfilePicture,
-                       NickName=memberinfo.NickName,
-                       Identity=memberinfo.Identity
-
-                       //WorkPictureID = workpictures.WorkPictureID,
-                       //SortNumber = workpictures.SortNumber,
-                       //WorkPicture = workpictures.WorkPicture
+                       MemberID = memberinfo.MemberID,
+                       ProfilePicture = memberinfo.ProfilePicture,
+                       NickName = memberinfo.NickName,
+                       Identity = memberinfo.Identity
                    };
         }
 
+        public IEnumerable<StudioViewModel> GetStudioInfoByMemberID(int MemberID)
+        {
+            var StudioinfoRepository = new GeneralRepository(new ProFinderContext());
+
+            return from mermberinfo in StudioinfoRepository.GetAll<MemberInfo>()
+                   
+                   select new StudioViewModel
+                   {
+                       MemberID = mermberinfo.MemberID,
+                       NickName = mermberinfo.NickName,
+                       Description = mermberinfo.Description,
+                       LocationID = mermberinfo.LocationID,
+                       //LogInTime = mermberinfo.LogInTime,
+                       ProfilePicture = mermberinfo.ProfilePicture,
+                       ExpertSubCategoryID = mermberinfo.SubCategoryID,
+                       Identity = mermberinfo.Identity
+                   };
+        }
+
+        public IEnumerable<StudioViewModel> GetStudioworksByMemberID(int MemberID)
+        {
+            var StudioworkRepository = new GeneralRepository(new ProFinderContext());
+
+            return from works in StudioworkRepository.GetAll<Works>()
+                   join memberinfo in StudioworkRepository.GetAll<MemberInfo>()
+                   on works.MemberID equals memberinfo.MemberID
+                   where works.MemberID == MemberID
+                   join workpictures in StudioworkRepository.GetAll<WorkPictures>()
+                   on works.WorkID equals workpictures.WorkID
+                   where workpictures.SortNumber==1
+                  
+                   select new StudioViewModel
+                   {
+                       WorkID = works.WorkID,
+                       WorkName = works.WorkName,
+                       WebsiteURL = works.WebsiteURL,
+                       WorkSubCategoryID = works.SubCategoryID,
+                       MemberID = memberinfo.MemberID,
+                       WorkPicture = workpictures.WorkPicture
+                   };
+        }
+
+        public IEnumerable<StudioViewModel> GetStudioQuotationByMemberID(int MemberID)
+        {
+            var StudioquotRepository = new GeneralRepository(new ProFinderContext());
+
+            return from quotation in StudioquotRepository.GetAll<Quotation>()
+                       //join memberinfo in StudioquotRepository.GetAll<MemberInfo>()
+                       //on quotation.MemberID equals memberinfo.MemberID
+                   where quotation.MemberID == MemberID
+                   join quotationpicture in StudioquotRepository.GetAll<OtherPicture>()
+                   on quotation.QuotationID equals quotationpicture.QuotationID
+
+
+                   select new StudioViewModel
+                   {
+                       QuotationId = quotation.QuotationID,
+                       //QuotationCategoryId = quotation.Category,要用sub反找cate  CategoryName
+                       QuotationSubcategoryId = quotation.SubCategoryID,
+                       Price = quotation.Price,
+                       Unit = quotation.QuotationUnit,
+                       QuotationImg = quotationpicture.MainPicture
+                       //用id去找name SubcategoryName 
+                       //WorkName = works.WorkName,
+                       //WebsiteURL = works.WebsiteURL,
+                       //WorkSubCategoryID = works.SubCategoryID,
+                       //MemberID = memberinfo.MemberID,
+                       //WorkPicture = workpictures.WorkPicture
+                   };
+
+        }
+
+        //評價
+    }
 
 
         //public IEnumerable<WorkPageViewModel> GetWorkdetailAndMemberinfoByWorkID(int WorkID)
