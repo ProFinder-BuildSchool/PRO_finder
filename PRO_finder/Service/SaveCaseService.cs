21ca6531b8096890150038f60f0a15f1746e4c83
christieee1106 <christieee1106@gmail.com>
2021-08-30 10:05:44 +0800

    christie0830

--- PRO_finder/Service/SaveCaseService.cs
+++ PRO_finder/Service/SaveCaseService.cs
@@ -17,13 +17,13 @@ namespace PRO_finder.Service
             _ctx = new GeneralRepository(new ProFinderContext());
         }
 
-        
+
 
         public List<SaveCaseViewModel> GetSaveCaseData(int MemberID)
         {
             List<SaveCaseViewModel> saveCases = new List<SaveCaseViewModel>();
-            var saved = _ctx.GetAll<SaveCase>().Where(x => x.MemberID == MemberID).ToList();    
-            foreach(var item in saved)
+            var saved = _ctx.GetAll<SaveCase>().Where(x => x.MemberID == MemberID).ToList();
+            foreach (var item in saved)
             {
                 Case theCase = _ctx.GetAll<Case>().FirstOrDefault(x => x.CaseID == item.CaseID);
                 saveCases.Add(new SaveCaseViewModel
@@ -55,5 +55,26 @@ namespace PRO_finder.Service
             _ctx.Create(entity);
             _ctx.SaveChanges();
         }
+
+        public void DeleItemFromSaveCase(int CaseID, int MemberID) 
+        {
+            
+            DateTime now = DateTime.UtcNow;
+            SaveCase entity = new SaveCase()
+            {
+                CaseID = CaseID,
+                SavedDate = now,
+                MemberID = MemberID
+
+            };
+            _ctx.Delete(entity);
+            _ctx.SaveChanges();
+        }
+
     }
+
 }
+
+
+        
+    
