a2aedf415071806cb6469eba5a7ab04c48acc89f
Chi-Ling-Lee <19940818jillian@gmail.com>
2021-09-03 13:33:01 +0800

    MyQuotationIndex 服務狀態

--- PRO_finder/APIControllers/CartController.cs
+++ PRO_finder/APIControllers/CartController.cs
@@ -41,7 +41,7 @@ namespace PRO_finder.APIControllers
             }
             
         }
-        //2.提取當前cart資料
+        //2.提取當前QuotationDetail cart資料
         public APIResult GetQuotationCart()
         {
             string result = "";
