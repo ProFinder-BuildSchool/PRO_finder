90d75077b17563dffc7c9be3965bba5065fe8c6b
Chi-Ling-Lee <19940818jillian@gmail.com>
2021-09-07 14:59:29 +0800

    Cloudinary 後端上傳調整

--- PRO_finder/Controllers/TalentController.cs
+++ PRO_finder/Controllers/TalentController.cs
@@ -67,7 +67,7 @@ namespace PRO_finder.Controllers
                     _quotaService.CreateOtherPics(quotationID, quotation.OtherPictureList);
                 }
 
-                return RedirectToAction("Index");
+                return RedirectToAction("MyQuotationIndex");
             }
             return View(quotation);
         }
@@ -317,9 +317,8 @@ namespace PRO_finder.Controllers
         public void UploadCloudinary()
         {
             HttpPostedFileBase file = Request.Files["picture"];
-            var result = _cloudinaryHelper.UploadCloudinaryAsync(file);
-            var url = JObject.Parse(result.Result);
-            Response.Write(url);
+            var result = _cloudinaryHelper.UploadToCloudinary(file);
+            Response.Write(result);
         }
 
        
